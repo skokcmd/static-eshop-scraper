0c0c4c1e9dee33310be75bfb22b95b9b2a63be0e
Boris Skok <skokcmd@gmail.com>
2022-09-04 18:00:59 +0200

    rename

--- src/main/java/com/skokcmd/StaticEshopScraper.java
+++ src/main/java/com/skokcmd/StaticEshopScraper.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
-public class ProfibaScraper {
+public class StaticEshopScraper {
     public static void main(String[] args) throws IOException {
         run();
     }
